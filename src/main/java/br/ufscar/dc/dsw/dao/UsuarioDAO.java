7ee317b5892f93d508832554c5f5d61982f35fe3
LeonardoAgnelo <leonardoagnelo2001@gmail.com>
2021-09-16 17:04:09 -0300

    update UsuarioDAO.java renomeado os atributos Usuario para usuario

--- src/main/java/br/ufscar/dc/dsw/dao/UsuarioDAO.java
+++ src/main/java/br/ufscar/dc/dsw/dao/UsuarioDAO.java
@@ -10,7 +10,7 @@ import br.ufscar.dc.dsw.domain.Usuario;
 
 public class UsuarioDAO extends GenericDAO{
 
-    public void insert(Usuario Usuario){
+    public void insert(Usuario usuario){
         String sql = "INSERT INTO Usuario (id, nome, email, senha tipo) VALUES (?, ?, ?, ?, ?) ";
 
         try{
@@ -18,11 +18,11 @@ public class UsuarioDAO extends GenericDAO{
             PreparedStatement statement = conn.prepareStatement(sql);
 
             statement = conn.prepareStatement(sql);
-            statement.setInt(1, Usuario.getId());
-            statement.setString(2, Usuario.getNome());
-            statement.setString(3, Usuario.getEmail());
-            statement.setString(4, Usuario.getSenha());
-            statement.setString(5, Usuario.getTipo());
+            statement.setInt(1, usuario.getId());
+            statement.setString(2, usuario.getNome());
+            statement.setString(3, usuario.getEmail());
+            statement.setString(4, usuario.getSenha());
+            statement.setString(5, usuario.getTipo());
             statement.executeUpdate();
 
             statement.close();
@@ -31,14 +31,14 @@ public class UsuarioDAO extends GenericDAO{
             throw new RuntimeException(e);
         }
     }
-    public void delete(Usuario Usuario){
+    public void delete(Usuario usuario){
         String sql = "DELETE FROM Usuario where id = ?";
 
         try{
             Connection conn = this.getConnection();
             PreparedStatement statement = conn.prepareStatement(sql);
 
-            statement.setInt(1, Usuario.getId());
+            statement.setInt(1, usuario.getId());
             statement.executeUpdate();
 
             statement.close();
