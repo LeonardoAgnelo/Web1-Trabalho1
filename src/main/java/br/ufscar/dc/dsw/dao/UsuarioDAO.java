d3516f95bc20dd956a76b10dccadc20cb7976980
LeonardoAgnelo <leonardoagnelo2001@gmail.com>
2021-09-16 16:51:49 -0300

    adiciona usuarioDAO.java

---
+++ src/main/java/br/ufscar/dc/dsw/dao/UsuarioDAO.java
@@ -0,0 +1,51 @@
+package br.ufscar.dc.dsw.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+
+import br.ufscar.dc.dsw.domain.Usuario;
+
+public class UsuarioDAO extends GenericDAO{
+
+    public void insert(Usuario Usuario){
+        String sql = "INSERT INTO Usuario (id, nome, email, senha tipo) VALUES (?, ?, ?, ?, ?) ";
+
+        try{
+            Connection conn = this.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement = conn.prepareStatement(sql);
+            statement.setInt(1, Usuario.getId());
+            statement.setString(2, Usuario.getNome());
+            statement.setString(3, Usuario.getEmail());
+            statement.setString(4, Usuario.getSenha());
+            statement.setString(5, Usuario.getTipo());
+            statement.executeUpdate();
+
+            statement.close();
+            conn.close();
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+        
+    }
+    public void delete(Usuario Usuario){
+        String sql = "DELETE FROM Usuario where id = ?";
+
+        try{
+            Connection conn = this.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            statement.setInt(1, Usuario.getId());
+            statement.executeUpdate();
+
+            statement.close();
+            conn.close();
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
