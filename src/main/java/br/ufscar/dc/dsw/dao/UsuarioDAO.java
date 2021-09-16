78defc23e57861d9e79ae8c950cdcba0d938fd6d
LeonardoAgnelo <leonardoagnelo2001@gmail.com>
2021-09-16 19:15:50 -0300

    uptade usuarioDAO

--- src/main/java/br/ufscar/dc/dsw/dao/UsuarioDAO.java
+++ src/main/java/br/ufscar/dc/dsw/dao/UsuarioDAO.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
-import java.sql.Statement;
 
 import br.ufscar.dc.dsw.domain.Usuario;
 
